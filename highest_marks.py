71d289e937011e7a535f9b70f2c010082db37928
prathameshpatil2 <prathameshpatil2344@gmail.com>
2026-09-18 14:20:39 +0530

    Day : LeetCode practice

---
+++ highest_marks.py
@@ -0,0 +1,17 @@
+class Student:
+
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+
+    def highest_marks(self):
+        highest = self.marks[0]
+
+        for mark in self.marks:
+            if mark > highest:
+                highest = mark
+
+        print("Highest Marks =", highest)
+
+s1 = Student("Prathamesh", [90,80,95])
+s1.highest_marks()

d779e159bc67005c418579ea7bb5178090a26f63
prathameshpatil2 <prathameshpatil2344@gmail.com>
2026-09-21 19:52:36 +0530

    Day : LeetCode practice

--- Account — Check Balance.py
+++ Account — Check Balance.py
@@ -7,3 +7,6 @@ class Account:
     def show_balance(self):
         print("Account No:", self.account_no)
         print("Balance:", self.balance)
+
+acc1 = Account("12345", 10000)
+acc1.show_balance()
